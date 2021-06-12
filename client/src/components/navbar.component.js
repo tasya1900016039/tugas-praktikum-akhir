a2eaf6554da0b8c942b8d3b0c9e59d0cca6a00f7
tasya1900016039 <tasya1900016039@webmail.uad.ac.id>
2021-06-12 14:36:38 +0700

    Install React-JS dan Membuat Layout Tampilan Aplikasi

---
+++ client/src/components/navbar.component.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">
+          Pelacak Latihan Mahasiswa Ahmad Dahlan
+        </Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">
+                Riwayat Latihan
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">
+                Buat Catatan Latihan Baru
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">
+                Buat Pengguna Baru
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
